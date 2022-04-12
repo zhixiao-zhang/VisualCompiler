45b01dbbb57b5ee985741e2a2c57a26d0811b99a
Patrick <Zhixiao-Zhang@outlook.com>
2022-04-12 19:50:23 +0800

    增加注释

--- include/compiler.h
+++ include/compiler.h
@@ -4,10 +4,14 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+//行数;
 extern int _yyline;
+//文字;
 extern char *yytext;
+//错误处理函数
 void yyerror(char *msg);
 
+//语法树
 typedef struct parserNode {
   int line;
   char *_tokenName;
@@ -18,15 +22,21 @@ typedef struct parserNode {
   };
 } *Ast, *pnode;
 
+//创建新的节点
 Ast newAst(char *name, int number, ...);
 
+//先序遍历语法树
 void PreOrder(Ast ast, int level);
 
+//节点个数
 int _nodeNum;
 
+//存放节点
 pnode nodeList[10000];
 int nodeChild[10000];
 
+//设置状态
 void Childset(pnode node);
 
+//是否有错误
 int hasFault;
