866f3eb70c27d1f22b559c59ca5b63c1431e36ab
Patrick <Zhixiao-Zhang@outlook.com>
2022-04-14 16:03:27 +0800

    添加注释

--- include/compiler.h
+++ include/compiler.h
@@ -13,19 +13,19 @@ void yyerror(char *msg);
 
 //语法树
 typedef struct parserNode {
-  int line;
-  char *_tokenName;
-  struct parserNode *fchild, *next;
+  int line; /*记录该节点在语法树中的行号*/
+  char *_tokenName; /*flex返回的token的名称*/
+  struct parserNode *fchild, *next; /*指向当前节点的指针和指向下一个兄弟节点的指针*/
   union  {
-    char *id_type;
-    int interval;
+    char *id_type; /*ID内容*/
+    int interval; /*INT的值*/
   };
 } *Ast, *pnode;
 
 //创建新的节点
 Ast newAst(char *name, int number, ...);
 
-//先序遍历语法树
+//先序遍历语法树(深度优先搜索)
 void PreOrder(Ast ast, int level);
 
 //节点个数
