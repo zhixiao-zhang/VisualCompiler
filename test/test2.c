90ace30ded3428aa8e7fd5adec167e30d251fc99
patrick <Zhixiao-Zhang@outlook.com>
2022-10-07 02:50:21 +0800

    merge from beta

---
+++ test/test2.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int a, b, c;
+    a = 3 + 4;
+    write(a);
+    b = 3 * 4;
+    write(b);
+    c = 3 - 4;
+    write(c);
+    while (c < a)
+        c = c + 1;
+    write(c);
+    return 0;
+}
